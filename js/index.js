20af60f76dc9be0644daaee9071227cf61acba48
Bianca Gonzalez <33680528+bfgonzalez@users.noreply.github.com>
2018-01-01 00:43:42 +0800

    Update
    Added functionality for chaining equations, and making use of the +/- sign should the user wish to start with a negative integer. Moreover, some visual changes have been made (i.e replaced the C and CE buttons with one button which clears all entries made so far), but kept the back button should the user which to only remove one element.

--- js/index.js
+++ js/index.js
@@ -1,63 +1,71 @@
-$(document).ready(function() {
-  var result = "";
-  var previous = 0;
-  var operation = ""; 
-  var current = "";
-   update(result);
+$(document).ready(function () {
+  var testLength = function (number) {
+    if (number.length > 14) {
+      number = "";
+      history.text("Max length reached");
+    }
+  };
   
-  $(".buttons").on("click", function() {
-    var buttonPressed = $(this).text();
-    console.log(buttonPressed);
-    
-      if (buttonPressed === "C") {
-          result = 0;
-          current = "";
-     }  else if (buttonPressed === "CE") {
-           current = "";
-     }  else if (buttonPressed === "Back") {
-           current = current.substring(0, current.length-1);
-     }  else if (buttonPressed === "+/-") {
-           current *= -1; 
-     }  else if (buttonPressed === ".") {
-           current += ".";
-     } else if (number(buttonPressed)) {
-        if (current === "0") current = buttonPressed;
-        else current = current + buttonPressed;
-     } else if (operator(buttonPressed)) {
-        previous = parseFloat(current);
-        operation = buttonPressed;
-        current = "";
-     } else if (buttonPressed === "=") {
-       current = operate(previous, current, operation);
-       operation = null;
-     }
-      
-        update(current);
+  var number = "";
+  var newNumber = "";
+
+  var history = $("#history"); 
+  var final = $("#output");
+  history.text(""); 
+  final.text("");
+  
+  $(".buttons").not("#clear-all, #clear-entry, #backspace, #equals, #plus, #minus, #multiply, #divide, #pos-neg").on("click", function() {
+    number += $(this).text();
+    history.text(number);
+    testLength(number);
   });
-
+  
+  $("#plus, #minus, #multiply, #divide").on("click", function() {
+    operator = $(this).text();
+    newNumber = number;
+    number += operator;
+    history.text(number);
+  });
+  // testing +/- functionality
+  $("#pos-neg").on("click", function () {
+    var currentSign = $("#history").text();
+    var newSign = currentSign === "" ? "-" : "";
+    history.text(number += newSign);
+  }); 
+  
+  $("#clear-all").on("click", function() {
+    number = "";
+    history.text("");
+    final.text("");
+    if ($(this).attr("id") === "clear-all") {
+      newNumber = "";
+    }
+    
+  });
+  
+  $("#backspace").on("click", function() {
+    if (number.length < 2) {
+      number = "";
+      history.text(""); 
+      final.text(""); 
+    }
+    number = number.slice(0, number.length-1);
+    history.text(number);
+    final.text("");
+  });
+  
+  $("#equals").on("click", function () {
+    if (operator) {
+      number = number.replace(/x/g, "*").replace(/÷/g, "/");
+      answer = eval(number);
+    }
+    if (answer.length > 14) {
+      answer = "Max length reached";
+    }
+    
+    final.text(answer.toFixed(2));
+    testLength(number);
+    
+    number = "";
+  });
 });
-
-update = function(displayOutput) {
-  var displayOutput = displayOutput.toString();
-  $("#output").text(displayOutput.substring(0, 10));
-  $("#history").text(displayOutput.substring(0, 10));
-};
-
-
-number = function(value) {
-  return !isNaN(value);
-};
-
-operator = function(value) {
-  return value === "/" || value === "x" || value === "+" || value === "-";
-};
-
-operate = function(a, b, operation) {
-  a = parseFloat(a); 
-  b = parseFloat(b); 
-  console.log(a, b, operation);
-  if (operation === "+") return a + b;
-  if (operation === "-") return a - b;
-  if (operation === "x") return a * b;
-  if (operation === "/") return a / b;
-};
