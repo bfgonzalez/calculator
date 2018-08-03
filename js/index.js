1b2b950c3302946ba9608eb0da9a8d94b5809d39
bfgonzalez <bfgonzalez142@gmail.com>
2018-08-03 10:50:51 +0800

    chore: code cleanup
    fixed structure and removed unnecessary white space

--- js/index.js
+++ js/index.js
@@ -5,35 +5,35 @@ $(document).ready(function () {
       history.text("Max length reached");
     }
   };
-  
+
   var number = "";
   var newNumber = "";
 
-  var history = $("#history"); 
+  var history = $("#history");
   var final = $("#output");
-  history.text(""); 
+  history.text("");
   final.text("");
-  
+
   $(".buttons").not("#clear, #backspace, #equals, #add, #subtract, #multiply, #divide, #pos-neg").on("click", function() {
     number += $(this).text();
     history.text(number);
     testLength(number);
   });
-  
+
   $("#add, #subtract, #multiply, #divide").on("click", function() {
     operator = $(this).text();
     newNumber = number;
     number += operator;
     history.text(number);
   });
-  
+
   // testing +/- functionality
   $("#pos-neg").on("click", function () {
     var currentSign = $("#history").text();
     var newSign = currentSign === "" ? "-" : "";
     history.text(number += newSign);
-  }); 
-  
+  });
+
   $("#clear").on("click", function() {
     number = "";
     history.text("");
@@ -41,20 +41,19 @@ $(document).ready(function () {
     if ($(this).attr("id") === "clear") {
       newNumber = "";
     }
-    
   });
-  
+
   $("#backspace").on("click", function() {
     if (number.length < 2) {
       number = "";
-      history.text(""); 
-      final.text(""); 
+      history.text("");
+      final.text("");
     }
     number = number.slice(0, number.length-1);
     history.text(number);
     final.text("");
   });
-  
+
   $("#equals").on("click", function () {
     if (operator) {
       number = number.replace(/x/g, "*").replace(/÷/g, "/");
@@ -63,10 +62,8 @@ $(document).ready(function () {
     if (answer.length > 14) {
       answer = "Max length reached";
     }
-    
     final.text(answer.toFixed(2));
     testLength(number);
-    
     number = "";
   });
 });
